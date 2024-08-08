2189838612917777df501929cd6f10276236000c
Pedro Alonso <pedro.alonsodeoliveira@hotmail.com>
2024-08-08 10:06:06 -0300

    ✨ Not found screen added

---
+++ src/pages/not-found/not-found.tsx
@@ -0,0 +1,10 @@
+import { ReactElement } from "react";
+import { Screen } from "../../components/screen/screen";
+
+export const NotFound = (): ReactElement => {
+  return (
+    <Screen>
+      <h1>404 - Página não encontrada</h1>
+    </Screen>
+  );
+};
