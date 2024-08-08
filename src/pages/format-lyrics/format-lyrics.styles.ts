d77059e833181a8160c5a321a2704d16a2dd1598
Pedro Alonso <pedro.alonsodeoliveira@hotmail.com>
2024-08-08 20:14:51 -0300

    ✨ Format lyrics page now working

--- src/pages/format-lyrics/format-lyrics.styles.ts
+++ src/pages/format-lyrics/format-lyrics.styles.ts
@@ -1,7 +1,31 @@
 import styled from "styled-components";
+import { fonts } from "../../assets/themes";
 
-export const Container = styled.div`
+export const Wrapper = styled.div`
   width: 100%;
   height: 100%;
   display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+  align-self: flex-start;
+  margin-top: 2rem;
+  gap: 1rem;
+`;
+
+export const ButtonWrapper = styled.div`
+  display: flex;
+  gap: 1rem;
+`;
+
+export const Title = styled.h1`
+  font-size: ${fonts.size.xxxl};
+`;
+
+export const Subtitle = styled.h2`
+  font-size: ${fonts.size.l};
+`;
+
+export const Caption = styled.p`
+  font-size: ${fonts.size.m};
 `;
