e82231f4b0de231c8656c8108550ca8cb36914db
Pedro Alonso <pedro.alonsodeoliveira@hotmail.com>
2024-08-14 15:04:23 -0300

    🚨 Small lint fixes

--- src/pages/format-lyrics/format-lyrics.tsx
+++ src/pages/format-lyrics/format-lyrics.tsx
@@ -1,22 +1,16 @@
-import { ReactElement, useEffect, useState } from "react";
-import { Screen } from "../../components/screen/screen";
-import { TextInput } from "../../components/text-input/text-input";
-import { Button } from "../../components/button/button";
-import { ButtonVariants } from "../../components/button/types";
-import { useNavigate } from "react-router-dom";
-import { SCREENS } from "../../assets/types";
-import {
-  ButtonWrapper,
-  Caption,
-  Subtitle,
-  Title,
-  Wrapper,
-} from "./format-lyrics.styles";
+import { ReactElement, useEffect, useState } from 'react';
+import { Screen } from '../../components/screen/screen';
+import { TextInput, TextInputVariants } from '../../components/text-input/text-input';
+import { Button } from '../../components/button/button';
+import { ButtonVariants } from '../../components/button/types';
+import { useNavigate } from 'react-router-dom';
+import { SCREENS } from '../../assets/types';
+import { ButtonWrapper, Caption, Subtitle, Title, Wrapper } from './format-lyrics.styles';
 
 export const FormatLyrics = (): ReactElement => {
   const navigate = useNavigate();
 
-  const [lyrics, setLyrics] = useState<string>("");
+  const [lyrics, setLyrics] = useState<string>('');
   const [hasCopied, setHasCopied] = useState<boolean>(false);
 
   const copyToClipboard = (value: string) => {
@@ -24,7 +18,7 @@ export const FormatLyrics = (): ReactElement => {
   };
 
   const onSubmit = (value: string) => {
-    const formattedLyrics = value.split("\n").join("\n\n");
+    const formattedLyrics = value.split('\n').join('\n\n');
     setLyrics(formattedLyrics);
     copyToClipboard(formattedLyrics);
     setHasCopied(true);
@@ -43,26 +37,15 @@ export const FormatLyrics = (): ReactElement => {
       <Wrapper>
         <Title>Formatar Letra</Title>
         <Subtitle>
-          Cole a letra que deseja formatar no campo abaixo e clique em
-          'Formatar'. Ela será formatada e copiada para sua área de
-          transferência.
+          Cole a letra que deseja formatar no campo abaixo e clique em 'Formatar'. Ela será formatada e
+          copiada para sua área de transferência.
         </Subtitle>
-        <TextInput value={lyrics} onChange={(text) => setLyrics(text)} />
+        <TextInput value={lyrics} onChange={(text) => setLyrics(text)} variant={TextInputVariants.TEXTAREA} />
         <ButtonWrapper>
-          <Button
-            type={ButtonVariants.PRIMARY}
-            onClick={() => onSubmit(lyrics)}
-            text="Formatar"
-          />
-          <Button
-            type={ButtonVariants.SECONDARY}
-            onClick={() => navigate(SCREENS.HOME)}
-            text="Voltar"
-          />
+          <Button type={ButtonVariants.PRIMARY} onClick={() => onSubmit(lyrics)} text='Formatar' />
+          <Button type={ButtonVariants.SECONDARY} onClick={() => navigate(SCREENS.HOME)} text='Voltar' />
         </ButtonWrapper>
-        {hasCopied && (
-          <Caption>Letra copiada para a área de transferência!</Caption>
-        )}
+        {hasCopied && <Caption>Letra copiada para a área de transferência!</Caption>}
       </Wrapper>
     </Screen>
   );
