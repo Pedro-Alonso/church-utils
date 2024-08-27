dab5908a4b8d1e274792a28370866eff5faaa8f7
Pedro Alonso <pedro.alonsodeoliveira@hotmail.com>
2024-08-27 17:04:36 -0300

    💄 Lyrics preview line break

---
+++ src/components/lyrics-preview/lyrics-preview.styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Container = styled.div``;
+
+export const Title = styled.h1``;
+
+export const Artist = styled.h2``;
+
+export const Lyrics = styled.p`
+  white-space: pre-line;
+`;
--- src/components/lyrics-preview/lyrics-preview.tsx
+++ src/components/lyrics-preview/lyrics-preview.tsx
@@ -9,10 +9,10 @@ interface ILyricsPreview {
 
 export const LyricsPreview = ({ useGetLyricsHook, title, artist, lyrics }: ILyricsPreview): ReactElement => {
   return (
-    <div onClick={useGetLyricsHook}>
-      <h1>{title}</h1>
-      <h2>{artist}</h2>
-      <p>{lyrics || 'Click to get lyrics'}</p>
-    </div>
+    <Container>
+      <Title>{title}</Title>
+      <Artist>{artist}</Artist>
+      <Lyrics>{lyrics || ''}</Lyrics>
+    </Container>
   );
 };
