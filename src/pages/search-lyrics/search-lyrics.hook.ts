13351b8b9583fb70791448cf6228787e9276274d
Pedro Alonso <pedro.alonsodeoliveira@hotmail.com>
2024-10-22 17:59:23 -0300

    🎨 Better search-lyrics structure

--- src/pages/search-lyrics/search-lyrics.hook.ts
+++ src/pages/search-lyrics/search-lyrics.hook.ts
@@ -1,7 +1,9 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { ISearchLyrics, SongData } from './search-lyrics';
 import { request } from '../../hooks/axios.config';
 import { sanitizeParam } from '../../utils/sanitize-param';
+import { sanitizeResponse } from '../../utils/sanitize-response';
+import { generateInterval } from '../../utils/generate-timer';
 
 export const useSearchLyrics = (): ISearchLyrics => {
   const [searchTerm, setSearchTerm] = useState('');
@@ -10,23 +12,54 @@ export const useSearchLyrics = (): ISearchLyrics => {
   const [songLyrics, setSongLyrics] = useState<string>('');
   const [currentSong, setCurrentSong] = useState<SongData | null>(null);
   const [isLoading, setIsLoading] = useState(false);
+  const intervalRef = useRef<NodeJS.Timeout | null>(null);
 
   const getResultsPreviewUrl = `http://api.genius.com/search?q=${searchTerm}&access_token=${clientAccessToken}`;
+  const serverUrl = process.env.REACT_APP_BACKEND_LOCAL_URL ?? '';
 
   useEffect(() => {
+    const requestAPI = async () => {
+      try {
+        const response = await request.get(serverUrl);
+        console.log('Server is running:', response.data);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        if (intervalRef.current) {
+          clearInterval(intervalRef.current);
+        }
+        const newTime = generateInterval(60 * 3000, 60 * 5000);
+        intervalRef.current = setTimeout(requestAPI, newTime);
+      }
+    };
+
+    requestAPI();
+
+    return () => {
+      if (intervalRef.current) {
+        clearInterval(intervalRef.current);
+      }
+    };
+  }, [serverUrl]);
+
+  useEffect(() => {
+    setResults([]);
+    const getResults = async () => {
+      try {
+        const response = await request.get(getResultsPreviewUrl);
+        setResults(response.data.response.hits);
+      } catch (error) {
+        console.error(error);
+      }
+    };
     if (searchTerm) {
-      request
-        .get(getResultsPreviewUrl)
-        .then((response) => {
-          setResults(response.data.response.hits);
-        })
-        .catch((error) => {
-          console.error(error);
-        });
-    } else setResults([]);
+      getResults();
+    }
   }, [getResultsPreviewUrl, searchTerm]);
 
   useEffect(() => {
+    console.log('Server URL:', serverUrl);
+    
     const getLyrics = async () => {
       const title = currentSong?.result.title || '';
       const artist = currentSong?.result.primary_artist.name || '';
@@ -37,14 +70,14 @@ export const useSearchLyrics = (): ISearchLyrics => {
         const sanitizedTitle = sanitizeParam(title);
         const sanitizedArtist = sanitizeParam(artist);
         const response = await request.get(
-          `http://127.0.0.1:5000/get-lyrics/${sanitizedArtist}/${sanitizedTitle}`,
+          `${serverUrl}/get-lyrics/${sanitizedArtist}/${sanitizedTitle}`,
           {
             headers: {
               'Client-access-token': clientAccessToken,
             },
           }
         );
-        const formattedLyrics = response.data.lyrics;
+        const formattedLyrics = sanitizeResponse(response.data.lyrics);
         setSongLyrics(formattedLyrics);
         console.log('Lyrics:', formattedLyrics);
       } catch (error) {
@@ -55,7 +88,7 @@ export const useSearchLyrics = (): ISearchLyrics => {
     if (currentSong) {
       getLyrics();
     }
-  }, [currentSong, clientAccessToken]);
+  }, [currentSong, clientAccessToken, serverUrl]);
 
   return {
     lyrics: songLyrics,
